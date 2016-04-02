f12e81631ee06888313aa133fd259e53c76f9bb3
Aaron Delgado <aaron.demu@gmail.com>
2016-04-02 00:04:51 -0600

    Add main Files

---
+++ Assets/Scripts/MobaMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class MobaMovement : MonoBehaviour
+{
+	private NavMeshAgent navMeshAgent;
+
+	// Use this for initialization
+	void Awake ()
+	{
+		navMeshAgent = GetComponent<NavMeshAgent> ();
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+		RaycastHit hit;
+		if (Input.GetButtonDown ("Fire2")) {
+			if (Physics.Raycast (ray, out hit, 100)) {
+
+				//mueve al personaje al punto que fue dado click
+				navMeshAgent.destination = hit.point;
+				navMeshAgent.Resume ();
+
+				if(hit.collider.CompareTag("Enemy")) {
+					//Hit a un enemigo
+					Debug.Log("hit");
+				}
+
+			}
+		}
+	}
+
+}
+
