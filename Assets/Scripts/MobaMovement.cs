2c19ac5350f53ee5111ee56ec253a9d764b16f2b
Aaron Delgado <aaron.demu@gmail.com>
2016-04-03 00:42:16 -0600

    Implementation of animator and states machine

--- Assets/Scripts/MobaMovement.cs
+++ Assets/Scripts/MobaMovement.cs
@@ -2,14 +2,17 @@
 using System.Collections;
 
 [RequireComponent(typeof(NavMeshAgent))]
-public class MobaMovement : MonoBehaviour
-{
-	private NavMeshAgent navMeshAgent;
+public class MobaMovement : MonoBehaviour{
 
+	private NavMeshAgent navMeshAgent;
+	public const string ANIM_WALK = "walk";
+
+	Animator anim;
 	// Use this for initialization
 	void Awake ()
 	{
 		navMeshAgent = GetComponent<NavMeshAgent> ();
+		anim = GetComponent<Animator> ();
 	}
 
 	// Update is called once per frame
@@ -18,11 +21,14 @@ public class MobaMovement : MonoBehaviour
 		Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
 		RaycastHit hit;
 		if (Input.GetButtonDown ("Fire2")) {
+			anim.SetBool ("walk", true);
 			if (Physics.Raycast (ray, out hit, 100)) {
+
 
 				//mueve al personaje al punto que fue dado click
 				navMeshAgent.destination = hit.point;
 				navMeshAgent.Resume ();
+
 
 				if(hit.collider.CompareTag("Enemy")) {
 					//Hit a un enemigo
@@ -30,6 +36,10 @@ public class MobaMovement : MonoBehaviour
 				}
 
 			}
+
+		}
+		if (Input.GetKeyDown (KeyCode.Alpha1)) {
+			
 		}
 	}
 
